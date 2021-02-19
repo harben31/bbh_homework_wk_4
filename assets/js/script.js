97b0bbf7e4ff77c801de04dc65d295d3a1fbeedc
Benjamin Harris <harben31@gmail.com>
2021-02-18 17:29:21 -0800

    added basic styling. Psuedocoded in js.

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//WHEN the start button is clicked 
+//THEN timer starts and first screen appears
+//WHEN a question is answer
+//THEN evaluates answer to see if it is correct and assign points and changes to next screen
+//WHEN all the questions have been answered 
+//THEN the highscore screen shows up and askes for initals
+//WHEN initals are entered and submitted
+//THEN the input value is paired with score value and stored in localstorage and displayed in the ul
+//WHEN reset highscore is clicked the localstorage is cleaned and score board is blank
